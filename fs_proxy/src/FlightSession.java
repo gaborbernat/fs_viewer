a6ca2aba089fefed438f2ef15e7f6b97f4c142ba
Gabor Bernat <bernat@primeranks.net>
2012-04-22 18:19:05 +0200

    Initial commit.

---
+++ fs_proxy/src/FlightSession.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: gabor.bernat
+ * Date: 4/22/12
+ * Time: 6:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FlightSession {
+
+}
